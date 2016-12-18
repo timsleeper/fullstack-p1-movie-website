79066d8d8cf48ae602e9865d61e1ae7cdf95e02b
Felipe Souza Lima <Felipe Souza Lima>
2016-12-18 18:39:54 -0200

    Modified to Top 10 IMDB

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,22 +1,18 @@
 import media
 import fresh_tomatoes
 
-top_1 = media.Movie('Shawshank Redemption',
+t1 = media.Movie('Shawshank Redemption',
                         'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
                         'https://images-na.ssl-images-amazon.com/images/M/MV5BODU4MjU4NjIwNl5BMl5BanBnXkFtZTgwMDU2MjEyMDE@._V1_UY1200_CR88,0,630,1200_AL_.jpg',
                         'https://www.youtube.com/watch?v=6hB3S9bIaco')
 
 
-#print(toy_story.storyline)
-#toy_story.show_trailer()
 
-top_2 = media.Movie('Indiana Jones',
-                        'Indiana searches for his father, a Holy Grail scholar, who has been kidnapped by Nazis.',
-                        'https://upload.wikimedia.org/wikipedia/en/f/fc/Indiana_Jones_and_the_Last_Crusade_A.jpg',
-                        'https://www.youtube.com/watch?v=a6JB2suJYHM')
+t2 = media.Movie('The Godfather',
+                        'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
+                        'https://s-media-cache-ak0.pinimg.com/736x/40/b4/21/40b421c7cfe1b3059d5e50d79e968270.jpg',
+                        'https://www.youtube.com/watch?v=sY1S34973zA')
 
-#print(indiana.storyline)
-#indiana.show_trailer()
 
-movies=[toy_story, indiana]
+movies=[t1, t2]
 fresh_tomatoes.open_movies_page(movies)
